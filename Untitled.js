451ca2d6d15d393389621359c06958deabbaa639
London R <londonxrange@gmail.com>
2017-08-08 16:35:50 +0000

    cal

---
+++ Untitled.js
@@ -0,0 +1,158 @@
+window.onload = function() {
+
+var current,
+    screen,
+    output,
+    limit,
+    zero,
+    period,
+    operator;
+    
+    screen = document.getElementById("display");
+
+var elem = document.querySelectorAll(".num");
+    
+      var len = elem.length;
+    
+      for(var i = 0; i < len; i++ ) {
+        
+        elem[i].addEventListener("click",function() {
+                  
+            num = this.value;
+                     
+            output = screen.innerHTML +=num;
+                  
+            limit = output.length;
+         
+         if(limit > 16 ) {
+        
+         alert("error");
+             
+       }
+       
+     },false);
+        
+    } 
+
+    document.querySelector(".zero").addEventListener("click",function() {
+        
+        zero = this.value;
+        
+        if(screen.innerHTML === "") {
+            
+           output = screen.innerHTML = zero;  
+        }
+        
+        else if(screen.innerHTML === output) {
+            
+         output = screen.innerHTML +=zero;
+            
+        }
+          
+    },false);
+    
+    document.querySelector(".period").addEventListener("click",function() {
+        
+        period = this.value;
+        
+        if(screen.innerHTML === "") {
+            
+         output = screen.innerHTML = screen.innerHTML.concat("0.");
+            
+         }
+    
+        else if(screen.innerHTML === output) {
+        
+          screen.innerHTML = screen.innerHTML.concat(".");
+            
+        }
+        
+    },false);
+    
+    
+    document.querySelector("#equal").addEventListener("click",function() {
+        
+      if(screen.innerHTML === output) {
+          
+        screen.innerHTML = eval(output);
+      }
+        
+      else {
+            screen.innerHTML = "";
+      }
+          
+    },false);
+    
+ document.querySelector("#back").addEventListener("click",function() {
+        
+        screen.innerHTML = "";
+        
+    },false);
+    
+   
+     var elem1 = document.querySelectorAll(".operator");
+    
+      var len1 = elem1.length;
+    
+      for(var i = 0; i < len1; i++ ) {
+        
+        elem1[i].addEventListener("click",function() {
+         
+        operator = this.value;
+         
+         if(screen.innerHTML === "") {
+            
+            screen.innerHTML = screen.innerHTML.concat("");
+            
+        }
+        
+        else if(output) {
+        
+            screen.innerHTML = output.concat(operator);
+            
+        }
+           
+    },false);
+          
+      }   
+}
+$(document).keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode === 49) {
+            $("#one").click();
+       } else if (keycode === 50) {
+            $("#two").click();
+        } else if (keycode === 51) {
+            $("#three").click();
+        } else if (keycode === 52) {
+            $("#four").click();
+        } else if (keycode === 53) {
+            $("#five").click();
+        } else if (keycode === 54) {
+            $("#six").click();
+        } else if (keycode === 55) {
+            $("#seven").click();
+        } else if (keycode === 56) {
+            $("#eight").click();
+        } else if (keycode === 57) {
+            $("#nine").click();
+        } else if (keycode === 48) {
+            $("#zero").click();
+        } else if (keycode === 97) {
+            $("#clear").click();
+        } else if (keycode === 99) {
+            $("#clear-entry").click();
+        } else if (keycode === 61) {
+            $("#equals").click();
+        } else if (keycode === 107) {
+            $("#plus").click();
+        } else if (keycode === 109) {
+            $("#minus").click();
+        } else if (keycode === 106 || keycode === 120) {
+            $("#times").click();
+        } else if (keycode === 111) {
+            $("#divison").click();
+        } else if (keycode === 110) {
+            $("#dec-point").click();}
+            
+           
